0e95b51ba2dbf4a707b73f8b6a0042a18c2b44f1
yuokada <yuokada@users.noreply.github.com>
2026-04-26 10:33:05 +0900

    Fix lint expectation in text details test

--- src/test/java/io/github/yuokada/subcommand/AnalyzeTextDetailsExtendedTest.java
+++ src/test/java/io/github/yuokada/subcommand/AnalyzeTextDetailsExtendedTest.java
@@ -37,6 +37,7 @@ class AnalyzeTextDetailsExtendedTest {
             WITH a AS (SELECT 1 AS id)
             SELECT LOWER(CAST(a.id AS VARCHAR)), COUNT(*) AS c, ROW_NUMBER() OVER(PARTITION BY a.id)
             FROM a JOIN catalog2.s.t2 ON 1=1
+            ORDER BY 1
             ;
             """;
         Files.writeString(sqlFile, sql);
